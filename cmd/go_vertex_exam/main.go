70e00024627f5769f228b57fd3ddca57b93a091f
kris <jimenezkristoffer.dev@gmail.com>
2025-04-23 22:17:21 +0800

    date handler

--- cmd/go_vertex_exam/main.go
+++ cmd/go_vertex_exam/main.go
@@ -1,16 +1,53 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
+type DateData struct {
+	Day  string
+	Date string
+}
+
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
-		fmt.Println(r.Body)
-	})
+	http.HandleFunc("/", dateHandler)
 
 	fmt.Println("Server started on port 8080")
 	http.ListenAndServe(":8080", nil)
+}
+
+func dateHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	err := r.ParseForm()
+
+	if err != nil {
+		http.Error(w, "Error parsing form", http.StatusBadRequest)
+		return
+	}
+
+	dateStr := r.FormValue("date")
+	layout := "02/01/2006" // matches "day/month/year"
+
+	// Parse the input string
+	parsedTime, err := time.Parse(layout, dateStr)
+	if err != nil {
+		http.Error(w, "Error parsing date:", http.StatusBadRequest)
+		return
+	}
+
+	dateThousandDays := parsedTime.AddDate(0, 0, 1000) // add date by 1000 days
+
+	data := DateData{
+		Day:  dateThousandDays.Format("Monday"), // format day of the week
+		Date: dateThousandDays.Format(layout),   // format back to DD/MM/YYYY
+	}
+
+	json.NewEncoder(w).Encode(data)
 }
